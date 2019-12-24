c9cefc30299319455fbc34c1e6132e15e4ccd365
MagicZL0 <tnwls9721@naver.com>
2019-12-24 18:53:04 +0900

    수정

--- Assets/Scripts/Game/TicTacTocManager.cs
+++ Assets/Scripts/Game/TicTacTocManager.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class TicTacTocManager : MonoBehaviour
 {
-    [SerializeField] Cell cell;
+    //플레이어의 Maker 타입
+    private MarkerType playerMarkerType;
 
-    void Start()
+    //현재 게임의 상태
+    private enum GameState { None, PlayerTurn, OpponentTurn, GameOver }
+
+    private GameState currentState;
+
+    private void Start()
     {
-        cell.MarkerType = MarkerType.None;
+        //임시코드
+        playerMarkerType = MarkerType.Circle;
+        currentState = GameState.PlayerTurn;
+    }
+    private void Update()
+    {
+        if (currentState == GameState.PlayerTurn || currentState == GameState.OpponentTurn)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector2 pos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+                RaycastHit2D hitinfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(pos), Vector2.zero);
+
+                if (hitinfo && hitinfo.transform.tag == "Cell")
+                {
+                    Cell cell = hitinfo.transform.GetComponent<Cell>();
+                    cell.MarkerType = playerMarkerType;
+
+                    if (currentState == GameState.PlayerTurn)
+                    {
+                        cell.MarkerType = playerMarkerType;
+                    }
+                    else
+                    {
+                        cell.MarkerType = (playerMarkerType == MarkerType.Circle) ? MarkerType.Cross : MarkerType.Circle;
+                    }
+
+                    currentState = (currentState == GameState.PlayerTurn) ? GameState.OpponentTurn : GameState.PlayerTurn;
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Game/Winner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Winner : MonoBehaviour
+{
+    GameObject[] cell;
+
+    Cell[][] cells;
+    MarkerType[] markerTypes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cell = GameObject.FindGameObjectsWithTag("Cell");
+        int k =0;
+        for (int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                cells[i][j] = cell[k].GetComponent<Cell>();
+                k++;
+            }
+        }
+        //cell = FindObjectsOfType<Cell>();  
+    }
+    private void OnApplicationQuit()
+    {
+        Win();
+    }
+    void Win()
+    {
+        //가로
+        for(int i=0; i<3; i++)
+        {
+            for (int j=0; j<3; j++)
+            {
+                if(j==2)
+                {
+                   // string gg = cell[i][j].MarkerType == cell[i][j-1].MarkerType ? cell[i][j-1].MarkerType == cell[i][j-2].MarkerType ? "Win" : null : null;
+                }
+                if(i==j)
+                {
+                     //markerTypes[i] = cell[i][j].MarkerType;
+                }
+            }
+        }
+        for(int i = markerTypes.Length; i<0; i--)
+        {
+            //string gg=  markerTypes[i] == markerTypes[i - 1] ? markerTypes[i - 1] == markerTypes[i - 2] ? "Win" : null : null;
+        }
+
+
+        //세로
+        for(int j=0; j < 3; j++)
+        {
+            for(int i =0; i<3; i++)
+            {
+                if(i==2)
+                {
+                   // string gg = cell[i][j].MarkerType == cell[i-1][j].MarkerType ? cell[i-1][j].MarkerType == cell[i-2][j].MarkerType ? "Win" : null : null;
+                }
+            }
+        }
+
+        //string gg= markerType[0] == markerType[1] ? markerType[1] == markerType[2] ? "Win" : null : null;
+    }
+}
