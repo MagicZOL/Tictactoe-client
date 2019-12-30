18b904aa637d9f613e437af675a91052b4852bc3
MagicZL0 <tnwls9721@naver.com>
2019-12-30 18:31:52 +0900

    클라이언트 수정

---
+++ Assets/Scripts/Game/ClientInfo.cs
@@ -0,0 +1,14 @@
+﻿//Room ID 와 Client ID
+public class ClientInfo 
+{
+    //readonly 값이 한번 할당되면 읽기 전용
+    //const는 선언과 동시에 할당을 해줘야 하는 차이점
+    public readonly string roomId;
+    public readonly string clientId;
+
+    public ClientInfo(string roomId, string clientId)
+    {
+        this.roomId = roomId;
+        this.clientId = clientId;
+    }
+}
---
+++ Assets/Scripts/Game/UI/GameOverPanelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameOverPanelManager : PanelManager
+{
+    [SerializeField] Text messageText; //확인버튼
+    
+    //확인버튼 메시지
+    public void SetMessage(string message)
+    {
+        messageText.text = message;
+    }
+
+    //확인버튼을 눌렀을때
+    public void OnclickComfirm(Button button)
+    {
+        button.interactable = false;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
